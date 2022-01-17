58199ca88584f8680e21c87d3fe47211ade825e5
tylerdurdenfc1 <96852428+tylerdurdenfc1@users.noreply.github.com>
2022-01-17 19:13:13 +1100

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,22 +1,28 @@
 <?php
 //get data from form  
+
+if (isset($_POST['submit'])) {
+
 $name = $_POST['name'];
 $info = $_POST['info'];
 $contact= $_POST['contact'];
 $task= $_POST['task'];
 
-$to = "tanaynagar4@gmail.com";
+$mailTo = "tanaynagar4@gmail.com";
+$headers = "From: ".$name;
 
-$subject = "Mail From collabnow";
-$txt ="Name = ". $name . "/r/n info = " . $info . "\r\nContact =" . $contact . "\r\nTask =" . $task;
+$txt ="Name = ". $name . "\r\n info = " . $info . "\r\nContact =" . $contact . "\r\nTask =" . $task;
 
-$headers = "From: noreply@collabnow.com" . "\r\n" .
+//$headers = "From: noreply@collabnow.com" . "\r\n" .
 
-"CC: somebodyelse@example.com";
+//"CC: somebodyelse@example.com";
 
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
 //redirect
-header("Location:businesses.html");
+
+header("Location:businesses.php?mailsend");
+}
+
 ?>                              
